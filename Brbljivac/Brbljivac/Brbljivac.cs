3dc82caf3e37d838ff304bcc8a56046597fc0aa1
neba88 <nebojsa.kalanj.developer@gmail.com>
2014-12-15 11:59:16 +0100

    Talk-back-er
    Talks back a few sentences

---
+++ Brbljivac/Brbljivac/Brbljivac.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Brbljivac
+{
+    public class Brbljivac
+    {
+        public void pitanja()
+        {
+            while (true)
+            {
+                Console.Write("kazi nesto:  ");
+                string nestoReceno = Console.ReadLine();
+
+                Console.WriteLine(nestoReceno);
+                Console.WriteLine(nestoReceno);
+                Console.WriteLine(nestoReceno);
+                Console.WriteLine(nestoReceno);
+            }
+            
+        }
+    }
+}
---
+++ Brbljivac/Brbljivac/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Brbljivac
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Brbljivac brb = new Brbljivac();
+            brb.pitanja();
+            Console.ReadLine();
+        }
+    }
+}
